b44f59d1d11710fb57c39a4966e9db0f3e98b154
MattGable <matthewgable@gmail.com>
2017-01-21 16:18:35 -0500

    Editing git/config

---
+++ LRDL2/src/LRDL2/Menu/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LRDL2.Menu
+{
+    public static class MainMenu
+    {
+       public static void Init ()
+       {
+            
+            while (true)
+            {
+                Console.WriteLine("############################################################");
+                Console.WriteLine("#                      --- LRDL2 ---       \u0066\u20AA    #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("# 'random' generates a random decimal number.              #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("# 'save' writes the content of your workspace.             #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("#                                                          #");
+                Console.WriteLine("############################################################");
+
+                Console.WriteLine("Please enter a choice from the above menu.");
+                try
+                {
+                    string userChoice = Console.ReadLine();
+
+                    if (userChoice.ToLower().Equals("random"))
+                    {
+                        Console.WriteLine("You picked random.");
+                    }
+                    else if (userChoice.ToLower().Equals("save"))
+                    {
+                        Console.WriteLine("You picked save.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+
+                Console.Clear();
+            }
+        }
+
+    }
+}
