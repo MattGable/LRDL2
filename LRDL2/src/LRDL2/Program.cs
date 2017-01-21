393d5e63d3a339d605361cf61427600f5b9853b8
MattGable <matthewgable@gmail.com>
2017-01-21 00:39:27 -0500

    Adding menus

---
+++ LRDL2/src/LRDL2/Program.cs
@@ -0,0 +1,16 @@
+﻿using LRDL2.menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LRDL2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainMenu.Init();
+        }
+    }
+}
---
+++ LRDL2/src/LRDL2/menu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LRDL2.menu
+{
+    public static class MainMenu
+    {
+       public static void Init ()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+
+    }
+}
